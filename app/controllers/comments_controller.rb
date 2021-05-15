3d4d9189632c7e5a9d53490f6387b1d25883905e
VolodymyrKostiv <volodymyrkostiv2001@gmail.com>
2021-05-15 16:33:13 +0300

    Fixed comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -17,7 +17,7 @@ class CommentsController < ApplicationController
   
   private
     def comment_params
-      params.require(:comment).permit(:body)
+      params.require(:comment).permit(:body, :article_id, :user_id)
     end
 end
   
